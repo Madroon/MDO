ee209608f1d5049e2e2432e81e8e8ce0c5c18db2
Madroon <nmnoordam@gmail.com>
2014-04-23 12:42:39 +0200

    23-4-2014 - worked on EMWET input
    Small changes also in aerosolver (changed back to two airfoils)

--- Assignment/Code/Main.m
+++ Assignment/Code/Main.m
@@ -8,7 +8,16 @@ close all;
 clear all;
 
 %% Set up for the optimizer
-
+% X(1) = b
+% X(2) = Croot
+% X(3) = lambda_inner
+% X(4) = lambda_outer
+% X(5) = sweepKink
+% X(6) = front spar location
+% X(7) = rear spar location
+% X(8) - X(..) = airfoil at root
+% X(..) - X(..) = airfoil at tip
+% Add surrogate variables
 %% Run the optimizer
 
 %% Plot the results
